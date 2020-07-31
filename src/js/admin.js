dc0ecac500f33f62d8a3d084a55e130a2b8d8fef
djpavle <pavledjuric@cityclean.rs>
2020-07-31 20:54:06 +0200

    login i dinamicko ucitavanje podataka

---
+++ src/js/admin.js
@@ -0,0 +1,33 @@
+
+function addNewPerson (name, surname) {
+  let persons = localStorage.getItem('persons')
+  if (persons == null) {
+    persons = []
+  } else {
+    persons = JSON.parse(persons)
+  }
+  persons.push({ name, surname })
+  localStorage.setItem('persons', JSON.stringify(persons))
+}
+
+function d (id) {
+  return document.getElementById(id)
+}
+
+document.getElementById('add-new-form').addEventListener('submit', function (event) {
+  event.preventDefault()
+  addNewPerson(d('name').value, d('surname').value)
+  d('name').value = ''
+  d('surname').value = ''
+})
+
+function getAllPersons () {
+  const dataFromStorage = d('dataFromLS')
+  const allPersons = JSON.parse(localStorage.getItem('persons'))
+  let output = '<tr>'
+  for (const person of allPersons) {
+    output += `<td>${person.id}</td><td>${person.name}</td><td>${person.surname}</td><td><button data-id="${person.id}" onclick="editPerson(${person.id})">Edit</button></td><td><button data-id="${person.id}" onclick="deletePerson(${person.id})">Delete</button><button data-id="${person.id}" onclick="viewPerson(${person.id})">View Profile</button></td></tr>`
+  }
+  dataFromStorage.innerHTML = output
+}
+getAllPersons()
---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+const submitButton = document.getElementById('login-submit-button')
+const usernameField = document.getElementById('username-field')
+const passwordField = document.getElementById('password-field')
+const errorMessage = document.getElementById('errorMessage')
+
+function checkIfUserIsValid (event) {
+  event.preventDefault()
+  const username = usernameField.value
+  const password = passwordField.value
+
+  for (const user of JSON.parse(localStorage.getItem('users'))) {
+    if (user.username === username && user.password === password) {
+      userType = user.typeOfUser
+      if (userType === 2) window.location.href = '/src/html/admin.html'
+      if (userType === 1) window.location.href == '/src/html/parent.html'
+    } else {
+      errorMessage.textContent = 'INVALID USERNAME/PASSWORD'
+    }
+  }
+}
+
+submitButton.addEventListener('click', checkIfUserIsValid)
